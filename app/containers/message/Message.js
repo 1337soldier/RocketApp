23eecf383f5cf494eb4faae6f5fbc05da2a9510d
huyhomie66 <huyhomie66@gmail.com>
2020-09-03 15:39:04 +0700

    reply refactor

--- app/containers/message/Message.js
+++ app/containers/message/Message.js
@@ -5,7 +5,6 @@ import Touchable from 'react-native-platform-touchable';
 
 import MessageContext from './Context';
 
-import User from './User';
 import styles from './styles';
 import RepliedThread from './RepliedThread';
 import MessageAvatar from './MessageAvatar';
@@ -22,160 +21,176 @@ import CallButton from './CallButton';
 import { themes } from '../../constants/colors'
 import database from "../../lib/database"
 
+const MessageContainer = (props) => {
+	return (
+		<View style={[styles.container, props.style, !props.isAuthor && { flexDirection: "row", alignItems: "center" }]}>
+			{props.children}
+		</View>
+	)
+}
+
+const Avatar = (props) => {
+	return (
+		<React.Fragment>
+			{!props.isAuthor &&
+				(<View style={{ width: 36 }} >
+					<MessageAvatar {...props} />
+				</View>)}
+		</React.Fragment>
+	)
+}
+
+const ContentContainer = ({ children, ...props }) => {
+	const flexRow = props.isAuthor ?
+		{ alignSelf: "flex-end", marginLeft: 48 }
+		: { alignSelf: "flex-start", marginRight: 48 }
+
+	return <View style={[styles.flex, flexRow]}>{children}</View>
+
+}
+
+
+const MessageInnerContainer = (props) => {
+	const authorStyle = props.isAuthor ? {
+		backgroundColor: themes[props.theme].messageBg,
+		borderWidth: 1,
+		borderColor: "#ccc",
+		paddingHorizontal: 10
+	} : {
+			marginTop: 8,
+			marginLeft: 10
+		}
+	return (
+		<View style={[styles.messageContent, authorStyle]}>
+			{props.children}
+		</View>
+
+	)
+}
+
+const MessageStatus = (props) => {
+	return (
+		<View><Text>dcm</Text></View>
+	)
+}
+
+const ReplyMessage = (props) => {
+	const [repliedUser, setRepliedUser] = useState({})
+
+	const getRepliedUser = async () => {
+		const db = database.active;
+		const msgCollection = db.collections.get('messages');
+		const isAuthorMsg = !props.tmid || !props.isHeader
+		let repliedUser = await msgCollection.find(isAuthorMsg ? props.id : props.tmid)
+		setRepliedUser(repliedUser.u)
+	}
+
+	useEffect(() => {
+		getRepliedUser()
+	}, [])
+
+	const thread = <RepliedThread  {...props} />;
+
+	return useMemo(() =>
+		(<View style={[styles.repliedUserThread, { borderColor: themes[props.theme].tintColor }]}>
+			<Text style={[props.isAuthor && { fontWeight: "500", fontSize: 16 }]}>
+				{repliedUser.username}
+			</Text>
+			{thread}
+		</View>
+		), [repliedUser])
+}
+
 const MessageInner = React.memo((props) => {
 
 	if (props.type === 'discussion-created') {
-		return (
-			<>
-				{/* <User {...props} /> */}
-				<Discussion {...props} />
-			</>
-		);
+		return <Discussion {...props} />
 	}
+
 	if (props.type === 'jitsi_call_started') {
 		return (
-			<>
-				{/* <User {...props} /> */}
+			<React.Fragment>
 				<Content {...props} isInfo />
 				<CallButton {...props} />
-			</>
+			</React.Fragment>
 		);
 	}
+
 	if (props.blocks && props.blocks.length) {
 		return (
-			<>
-				{/* <User {...props} /> */}
+			<React.Fragment>
 				<Blocks {...props} />
 				<Thread {...props} />
 				<Reactions {...props} />
-			</>
+			</React.Fragment>
 		);
 	}
 
 
 	return (
-		<>
-			{/* <User {...props} /> */}
+		<React.Fragment>
 			<Content {...props} />
 			<Attachments {...props} />
 			<Urls {...props} />
 			<Thread {...props} />
 			<Reactions {...props} />
 			<Broadcast {...props} />
-		</>
+		</React.Fragment>
 	);
 });
 MessageInner.displayName = 'MessageInner';
 
+
+const Container = ({ children, restProps }) => {
+	return (
+		<MessageContainer {...restProps}>
+			<Avatar {...restProps} />
+			<ContentContainer {...restProps}>
+				<MessageInnerContainer {...restProps}>
+					{children}
+				</MessageInnerContainer>
+				<ReadReceipt {...restProps} />
+			</ContentContainer>
+		</MessageContainer >
+
+	)
+}
+
 const Message = React.memo((props) => {
-	let isMainUser = props.user.id === props.author._id && !props.isInfo;
-	let isReplied = props.isThreadReply || props.isThreadSequential;
-
-	const mainUserBg = isMainUser ? {
-		backgroundColor: themes[props.theme].messageBg,
-		borderWidth: 1,
-		borderColor: "#ccc",
-		paddingHorizontal: 10
-	} : {
-			marginLeft: 10
-		}
-
-	const flexRow = isMainUser ?
-		{ alignSelf: "flex-end", marginLeft: 48 }
-		: { alignSelf: "flex-start", marginRight: 48 }
+	let isAuthor = props.user.id === props.author._id && !props.isInfo;
+	const restProps = { isAuthor, ...props }
 
 	if (props.isInfo) {
 		return (
 			<View style={[styles.container, props.style, styles.center, { flexDirection: "row" }]}>
 				<MessageAvatar small {...props} />
-				<View
-					style={[
-						styles.messageContent,
-						styles.messageContentWithHeader
-					]}
-				>
-					<Content {...props} isMainUser={isMainUser} />
+				<View style={[styles.messageContent, styles.messageContentWithHeader]}>
+					<Content {...props} />
 				</View>
 			</View>
-
 		)
 	}
 
 	if (props.isThreadReply || props.isThreadSequential) {
 
-		const [repliedUser, setRepliedUser] = useState({})
-
-		const getRepliedUser = async () => {
-			const db = database.active;
-			const msgCollection = db.collections.get('messages');
-			const isAuthor = !props.tmid || !props.isHeader
-			let repliedUser = await msgCollection.find(isAuthor ? props.id : props.tmid)
-			setRepliedUser(repliedUser.u)
-		}
-
-		useEffect(() => {
-			getRepliedUser()
-		}, [])
-
-		const thread = <RepliedThread  {...props} />
 		return (
-			<View style={[styles.container, props.style]}>
-				<View style={[styles.flex, flexRow]}>
-					{!isMainUser && <MessageAvatar {...props} />}
-					<View style={[styles.messageContent, mainUserBg]}>
-						<View style={[styles.repliedUserThread, {
-							borderColor: themes[props.theme].tintColor,
-						},
-						]}>
-							{useMemo(() => (
-								<Text style={[isMainUser && { fontWeight: "500", fontSize: 16 }]}>
-									{repliedUser.username}
-								</Text>
-							), [repliedUser])}
-							{thread}
-						</View>
-						<Content {...props} isMainUser={isMainUser} />
-					</View>
-					<ReadReceipt
-						isReadReceiptEnabled={props.isReadReceiptEnabled}
-						unread={props.unread}
-						theme={props.theme}
-					/>
-				</View>
-			</View >
+			<Container restProps={restProps}>
+				<Content {...props} />
+			</Container>
+
 		);
 	}
 
 	return (
-		<View style={[styles.container, props.style, !isMainUser && { flexDirection: "row" }]}>
-			{!isMainUser &&
-				(
-					<View style={{ width: 36 }} >
-						<MessageAvatar {...props} />
-					</View>)
-			}
+		<Container restProps={restProps}>
+			<MessageInner {...restProps} />
+		</Container>
 
-			<View style={[flexRow]}>
-				<View
-					style={[
-						styles.messageContent,
-						mainUserBg,
-						props.isHeader && styles.messageContentWithHeader
-					]}
-				>
-					{<MessageInner {...props} isMainUser={isMainUser} />}
-				</View>
-				<ReadReceipt
-					isReadReceiptEnabled={props.isReadReceiptEnabled}
-					unread={props.unread}
-					theme={props.theme}
-				/>
-			</View>
-		</View >
 	);
 });
 
 Message.displayName = 'Message';
+
 
 const MessageTouchable = React.memo((props) => {
 	if (props.hasError) {
@@ -183,18 +198,19 @@ const MessageTouchable = React.memo((props) => {
 			<View>
 				<Message {...props} />
 			</View>
+
 		);
 	}
+
 	const { onLongPress } = useContext(MessageContext);
 	return (
 		<Touchable
 			onLongPress={onLongPress}
 			disabled={props.isInfo || props.archived || props.isTemp}
 		>
-			<View>
-				<Message {...props} />
-			</View>
+			<Message {...props} />
 		</Touchable>
+
 	);
 });
 MessageTouchable.displayName = 'MessageTouchable';
