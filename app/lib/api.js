65095cf94827e0438442dcf1f05760b5f80fda35
huyhomie66 <huyhomie66@gmail.com>
2020-09-21 11:03:16 +0700

    test remote

--- app/lib/api.js
+++ app/lib/api.js
@@ -3,6 +3,6 @@ import axios from "./axiosInstance"
 const verifyOTP = async ({ phone, otp }) => await axios.post("/verifyOTP", { phone, otp })
 
 
-const getOTP = async () => await axios.post('/getOTP')
+const getOTP = async ({ }) => await axios.post('/getOTP')
 
 export { verifyOTP, getOTP }
